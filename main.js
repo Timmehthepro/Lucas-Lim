c21c0104c684697ac486565c072192f26fc22d77
Timmehthepro <125130270+Timmehthepro@users.noreply.github.com>
2024-08-31 11:33:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function b1(){
+    window.open("https://code.org/")
+}
+function project(){
+    window.open("project.html")
+}
+function b2(){
+window.open("https://codepen.io/")
+}
+function b3(){
+    window.open("https://appinventor.mit.edu/")
+}
+function b4(){
+    window.open("https://dialogflow.cloud.google.com/#/agent/bhumi-sassistant-cdsv/intents")
+}
+function b5(){
+    window.open("https://code.visualstudio.com/")
+}
+
+function home(){
+    window.open("index.html")
+}
+
